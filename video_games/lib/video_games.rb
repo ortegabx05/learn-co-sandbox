74788a28444758410f9534810f53ac3075728cd3
jessi ortega <ortegabx05@gmail.com>
2019-09-27 03:51:24 +0000

    added require_relatives

---
+++ video_games/lib/video_games.rb
@@ -0,0 +1,7 @@
+require_relative "./video_games/version"
+require_relative "./video_games/cli"
+
+module VideoGames
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ video_games/lib/video_games/cli.rb
@@ -0,0 +1,7 @@
+
+class VideoGames::CLI
+  def call
+    puts 'Hello, here is a list of the current upcoming video games!'
+  end
+  
+end
